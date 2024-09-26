2c101559168fe976fda4fa99eb332fe2d69d2279
derossca <derossca@mail.uc.edu>
2024-09-26 10:37:06 -0400

    made fortunetellerviewer

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,2 +1,9 @@
-public class FortuneTellerFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class FortuneTellerFrame extends JFrame {
+
+    public FortuneTellerFrame() {
+
 }
--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -1,5 +1,24 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
 public class FortuneTellerViewer {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Fortune Teller");
+
+        //Centering and sizing the fram
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+
+        int width = screenSize.width * (3/4);
+        int height = screenSize.height * (3/4);
+        frame.setSize(width, height);
+
+        int x = (screenSize.width - width) / 2;
+        int y = (screenSize.height - height) / 2;
+        frame.setLocation(x, y);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
