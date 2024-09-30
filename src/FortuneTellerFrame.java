0c6dfe275a0bb9354259dbda6950677d010ad822
derossca <derossca@mail.uc.edu>
2024-09-30 08:03:31 -0400

    working on FortuneTellerFrame panels

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,9 +1,53 @@
 import javax.swing.*;
 import java.awt.*;
-import java.net.URL;
 
 public class FortuneTellerFrame extends JFrame {
 
+    JPanel mainPnl;
+    JPanel topPnl;
+    JPanel middlePnl;
+    JPanel bottomPnl;
+
+    JTextArea displayTA;
+    JScrollPane scroll;
+
+    JLabel titlelbl;
+    ImageIcon icon;
+
+    JButton fortuneBtn;
+    JButton quitBtn;
+
     public FortuneTellerFrame() {
 
+        mainPnl = new JPanel();
+        mainPnl.setLayout(new BorderLayout());
+        createTopPanel();
+        mainPnl.add(topPnl, BorderLayout.NORTH);
+        createMiddlePanel();
+        mainPnl.add(middlePnl, BorderLayout.CENTER);
+        createBottomPanel();
+        mainPnl.add(bottomPnl, BorderLayout.SOUTH);
+
+        add(mainPnl);
+        setTitle("Fortune Teller");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private void createTopPanel() {
+
+    }
+
+    private void createMiddlePanel() {
+        middlePnl = new JPanel();
+        displayTA = new JTextArea(10, 100);
+        scroll = new JScrollPane(displayTA);
+        middlePnl.add(scroll);
+    }
+
+    private void createBottomPanel() {
+        fortuneBtn = new JButton("Read My Fortune!");
+        quitBtn = new JButton("Quit");
+    }
 }
--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -4,6 +4,6 @@ import java.awt.*;
 public class FortuneTellerViewer {
 
     public static void main(String[] args){
-
+        JFrame frame = new JFrame("Fortune Teller");
     }
 }
